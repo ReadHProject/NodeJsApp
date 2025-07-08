230ab76e11c29df4a88f1fe209f99af1ff8273c7
Anky <ankit8080dhanare@gmail.com>
2025-07-08 18:55:49 +0530

    correcting cart model

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,86 +1,29 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema(
+const cartItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Products",
+    required: true,
+  },
+  name: { type: String, required: true },
+  image: { type: String },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true, default: 1 },
+});
+
+const cartSchema = new mongoose.Schema(
   {
-    shippingInfo: {
-      address: {
-        type: String,
-        required: [true, "Address is required"],
-      },
-      city: {
-        type: String,
-        required: [true, "City is required"],
-      },
-      country: {
-        type: String,
-        required: [true, "Country is required"],
-      },
-    },
-    orderItems: [
-      {
-        name: {
-          type: String,
-          required: [true, "Product name is required"],
-        },
-        price: {
-          type: Number,
-          required: [true, "Product price is required"],
-        },
-        quantity: {
-          type: Number,
-          required: [true, "Product quantity is required"],
-        },
-        images: {
-          type: String,
-          required: [true, "Product image is required"],
-        },
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Products",
-          required: [true, "Product is required"],
-        },
-      },
-    ],
-    paymentMethod: {
-      type: String,
-      enum: ["COD", "ONLINE"],
-      default: "COD",
-    },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Users",
-      required: [true, "User is required"],
+      required: true,
+      unique: true, // Ensure one cart per user
     },
-    paidAt: Date,
-    paymentInfo: {
-      id: String,
-      status: String,
-    },
-    itemPrice: {
-      type: Number,
-      required: [true, "Item price is required"],
-    },
-    tax: {
-      type: Number,
-      required: [true, "Tax price is required"],
-    },
-    shippingCharges: {
-      type: Number,
-      required: [true, "Shipping Charges is required"],
-    },
-    totalAmount: {
-      type: Number,
-      required: [true, "Total Amount is required"],
-    },
-    orderStatus: {
-      type: String,
-      enum: ["processing", "shipped", "delivered"],
-      default: "processing",
-    },
-    deliveredAt: Date,
+    items: [cartItemSchema],
   },
   { timestamps: true }
 );
 
-export const orderModel = mongoose.model("Orders", orderSchema);
-export default orderModel;
+export const cartModel = mongoose.model("Cart", cartSchema);
+export default cartModel;
